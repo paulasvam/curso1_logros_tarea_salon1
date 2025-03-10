bd815e37edeaf25eba7b178278d520b7cc41963e
paulasvam <anapaulacv2004@gmail.com>
2025-03-10 11:21:17 -0400

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,5 @@
+ejercicio 1
+nombre= "paula"
+edad= 21
+ciudad= "mara"
+print(f"hola mi nombre es {nombre}, tengo {edad} años y vivo en el municipio {ciudad}")
---
+++ ejercicio2.py
@@ -0,0 +1,7 @@
+ejercicio 2
+nombre= "paula"
+materia1= 15
+materia2= 19
+materia3= 17
+promedio= (materia1+materia2+materia3)/3
+print(f"el promeio de la estudiante {nombre} es de {promedio} puntos")
